666e73c6235546b05b8e85da7485c945e0116052
Ural Kozhokmatov <ural46@gmail.com>
2023-04-03 23:59:12 +0000

    finish celery intro

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -50,7 +50,11 @@ class Dev(Configuration):
         'django.contrib.staticfiles',
         'movies',
         'gh',
+        'django_celery_results',
     ]
+    
+    CELERY_RESULT_BACKEND = "django-db"
+    CELERY_BROKER_URL = "redis://localhost:6379/0"
 
     MIDDLEWARE = [
         'django.middleware.security.SecurityMiddleware',
